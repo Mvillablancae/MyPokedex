604936fbfcac998fa1ab88d740f243a54df203c7
MVillablanca <marcelo.villablancae@sansano.usm.cl>
2023-07-28 22:29:33 -0400

    Definition of PokemonApiService service to fetch data from GET endpoints

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MyPokedex
-A pokedex application that shows list's of pokemons and it's details
+A pokedex application that shows list's of Pokemons and it's details
+
+
+Resources used:
+
+Json to Kotlin converter to define data classes using SerializedName of GJson
+https://json2kt.com/
--- app/src/main/java/api/PokemonApiService.kt
+++ app/src/main/java/api/PokemonApiService.kt
@@ -1,4 +1,22 @@
 package api
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Path
+
+
+import model.PokemonDetailResponse
 
 interface PokemonApiService {
+    @GET("pokemon")
+    suspend fun getPokemonList(
+        @Query("offset") offset: Int,
+        @Query("limit") limit: Int
+    ): ArrayList<PokemonDetailResponse>
+
+    @GET("pokemon/{name}")
+    suspend fun getPokemonDetail(@Path("name") name: String): PokemonDetailResponse
+
+    @GET("pokemon/{id}")
+    suspend fun getPokemonDetail(@Path("id") id: Int): PokemonDetailResponse
+
 }
