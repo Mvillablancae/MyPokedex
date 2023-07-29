235d6c15ddd9b8317bf29fbf4002d6972a17100a
MVillablanca <marcelo.villablancae@sansano.usm.cl>
2023-07-29 18:41:18 -0400

    Manejo de errores

--- app/src/main/java/ui/pokedex/PokedexListActivity.kt
+++ app/src/main/java/ui/pokedex/PokedexListActivity.kt
@@ -3,20 +3,23 @@ package ui.pokedex
 import Network.PokeAPIClient
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import android.widget.AbsListView
 import android.widget.ListView
-import kotlinx.coroutines.Dispatchers
+import android.widget.ProgressBar
+import android.widget.Toast
 
+//Manejo de errores
+import java.lang.Exception
+import java.io.IOException
+
+//Manejo de hilos
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.lang.Exception
-
-//
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
-
-import model.PokemonDetailResponse
 
 import com.example.mypokedex.R
 import model.Pokemon
@@ -24,6 +27,7 @@ import model.Pokemon
 class PokedexListActivity : Activity() {
 
     private lateinit var listView: ListView
+    private lateinit var progressBar: ProgressBar
     private lateinit var pokemonAdapter: PokemonDetailsAdapter
     private val pokemonList = mutableListOf<Pokemon>()
     private var isLoading = false
@@ -32,11 +36,13 @@ class PokedexListActivity : Activity() {
     // Declarar una propiedad para el CoroutineScope
     private val coroutineScope: CoroutineScope = MainScope()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokedex)
 
         listView = findViewById(R.id.listView)
+        progressBar = findViewById(R.id.progressBar)
         pokemonAdapter = PokemonDetailsAdapter(this, pokemonList)
         listView.adapter = pokemonAdapter
 
@@ -47,7 +53,7 @@ class PokedexListActivity : Activity() {
                 visibleItemCount: Int,
                 totalItemCount: Int
             ) {
-                if (!isLoading && totalItemCount <= (firstVisibleItem + VISIBLE_THRESHOLD)) {
+                if ((!isLoading && totalItemCount <= (firstVisibleItem + VISIBLE_THRESHOLD )) && offset != 0) {
                     // Cargar más elementos cuando el usuario llega al final de la lista
                     loadMorePokemon()
                 }
@@ -62,13 +68,10 @@ class PokedexListActivity : Activity() {
 
     private fun loadMorePokemon() {
         isLoading = true
-
+        progressBar.visibility = View.VISIBLE
         coroutineScope.launch(Dispatchers.Main) {
             try {
                 val response = withContext(Dispatchers.IO) {
-                    // Realizar la llamada a la API utilizando Retrofit y coroutines
-                    // Aquí debes usar tu propia URL de la API y adaptar el código para obtener los datos
-
                     val apiService = PokeAPIClient.pokemonApiService
                     apiService.getPokemonList(offset, LIMIT) // Llama a tu función de la API
                 }
@@ -80,13 +83,22 @@ class PokedexListActivity : Activity() {
                     pokemonAdapter.notifyDataSetChanged()
                     offset += newPokemonList.size
                 } else {
-                    // Manejar el error de la API
+                    Toast.makeText(
+                        this@PokedexListActivity,
+                        "Error en el servidor, inténtelo de nuevo más tarde.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
+            }catch (e: IOException) {
+                    // Error de conexión de red
+                    Toast.makeText(this@PokedexListActivity, "Error de conexión de red", Toast.LENGTH_SHORT).show()
+
             } catch (e: Exception) {
-                println(e)
+                Toast.makeText(this@PokedexListActivity, "Error en el servidor, inténtelo de nuevo más tarde.", Toast.LENGTH_SHORT).show()
             }
 
             isLoading = false
+            progressBar.visibility = View.GONE
         }
     }
 
