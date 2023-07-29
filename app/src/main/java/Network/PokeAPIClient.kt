4222c8953290503072cd5cc619d9d81f5e78bcf5
MVillablanca <marcelo.villablancae@sansano.usm.cl>
2023-07-28 23:11:19 -0400

    Singleton for PokeAPIClient

---
+++ app/src/main/java/Network/PokeAPIClient.kt
@@ -0,0 +1,22 @@
+package Network
+
+import api.PokemonApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//Singleton de APIClient
+object PokeAPIClient {
+    private const val BASE_URL = "https://pokeapi.co/api/v2/"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val pokemonApiService: PokemonApiService by lazy {
+        retrofit.create(PokemonApiService::class.java)
+    }
+
+}
--- app/src/main/java/NetworkClient/NetworkClient.kt
+++
@@ -1,4 +0,0 @@
-package NetworkClient
-
-class NetworkClient {
-}
